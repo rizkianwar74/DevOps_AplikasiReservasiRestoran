29c478298fbd75f9d1564d63c1bf7cf60dda11f9
rizkianwar74 <77889962+rizkianwar74@users.noreply.github.com>
2025-11-06 22:18:05 +0700

    Update init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -1,7 +1,3 @@
--- Initial database setup
--- This file is optional and will be executed when MySQL container first starts
-
--- Create database if not exists (already handled by docker-compose)
 CREATE DATABASE IF NOT EXISTS resto_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Grant privileges
@@ -10,6 +6,3 @@ FLUSH PRIVILEGES;
 
 -- Switch to database
 USE resto_db;
-
--- Add any initial SQL setup here if needed
--- Tables will be created by Sequelize migrations
